16e689185c92cb33c383e73e5c003479ea93c6a9
GnanasakthiT <hello.this.is.gst@gmail.com>
2026-07-16 06:59:29 +0530

    Linked List

--- PolynomialManipulation.cpp
+++ PolynomialManipulation.cpp
@@ -16,32 +16,66 @@ struct node{
 
 struct node* temp = NULL;
 
-void insert(int val, int power, struct node* ptr){
-    ptr->next = new node(val, power);
-    ptr = ptr->next;
+void insert(int val, int power, struct node* &ptr){
+    if(ptr == NULL){
+        ptr = new node(val, power);
+        return;
+    }
+
+    temp = ptr;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = new node(val, power);
 
 }
 
-void addPoly(struct node* p1, struct node* p2){
+struct node* addPoly(struct node* p1, struct node* p2){
+    struct node* final = NULL;
 
+    while(p1 != NULL && p2 != NULL){
+        if(p1->pow == p2->pow){
+            insert(p1->coeff + p2->coeff, p1->pow, final);
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        else if(p1->pow > p2->pow){
+            insert(p1->coeff, p1->pow, final);
+            p1 = p1->next;
+        }
+        else{
+            insert(p2->coeff, p2->pow, final);
+            p2 = p2->next;
+        }
+    }
+
+    while(p1 != NULL){
+        insert(p1->coeff, p1->pow, final);
+        p1 = p1->next;
+    }
+
+    while(p2 != NULL){
+        insert(p2->coeff, p2->pow, final);
+        p2 = p2->next;
+    }
+
+    return final;
 }
 
 void display(struct node* ptr){
     temp = ptr;
-    while(temp){
-        cout << temp->coeff << " x^ " << temp->pow << " + ";
+    while(temp->next){
+        cout << temp->coeff << " x^" << temp->pow << " + ";
+        temp = temp->next;
     }
+    cout << temp->coeff;
 }
 
 int main(){
     int eachVal, eachPow, len;
 
-    struct node* equation1 = NULL;
-    struct node* equation2 = NULL;
-
-
-    struct node* poly1 = equation1;
-    struct node* poly2 = equation2;
+    struct node* poly1 = NULL;
+    struct node* poly2 = NULL;
     
     cout << "Enter the number of terms in the first polynomial: ";
     cin >> len;
@@ -64,13 +98,15 @@ int main(){
     }
     
     cout << "The first polynomial is:" << endl;
-    display(equation1);
+    display(poly1);
+    cout << endl;
 
     cout << "The second polynomial is:" << endl;
-    display(equation2);
+    display(poly2);
+    cout << endl;
 
-    // struct node* result = addPoly(equation1, equation2);
+    struct node* result = addPoly(poly1, poly2);
 
-    // cout << "The resultant polynomial is:" << endl;
-    // display(result);
+    cout << "The resultant polynomial is:" << endl;
+    display(result);
 }
